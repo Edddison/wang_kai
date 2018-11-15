d8534ee008211c361dc4f1821381787ab3ed0f88
PC-20170717IYTP\Administrator <461718159@qq.com>
2018-11-15 11:34:25 +0800

    5.5  1.0

---
+++ 5.5/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int total=0;
+    int figure=1,counter=0,number=0;
+    for(cin>>counter; figure<=counter; ++figure,cin>>number)
+
+
+    total=total+number;
+    cout<<"total is:"<<total;
+}
