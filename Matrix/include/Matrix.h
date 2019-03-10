68aeedeace91832db10ec57f6c92adb7c1be065c
PC-20170717IYTP\Administrator <461718159@qq.com>
2019-03-10 19:42:09 +0800

    10矩阵

---
+++ Matrix/include/Matrix.h
@@ -0,0 +1,18 @@
+
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <iostream>
+
+class Matrix
+{
+
+    friend &operator+(Matrix &,Matrix &);
+
+    public:
+          double Matrix(int ,int);
+
+
+};
+
+#endif // MATRIX_H
